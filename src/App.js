9d1e36f0d7818e75bf66e28e88412734d4cb6bd6
Andriikryl <andreyk557@gmail.com>
2023-06-05 19:43:16 +0300

    add componennts for structer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Nav from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
 function App() {
-  return <h1>asd</h1>;
+  return (
+    <>
+      <Nav />
+      <Products />
+      <Recommended />
+    </>
+  );
 }
 
 export default App;
