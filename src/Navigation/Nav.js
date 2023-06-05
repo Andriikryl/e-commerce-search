318f8383b954791762bbdf5382e858bcf2a09ebb
Andriikryl <andreyk557@gmail.com>
2023-06-05 19:55:17 +0300

    add to nav-component input and buttons

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,6 +1,29 @@
 import React from "react";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your search shoes"
+          className="search-input"
+        ></input>
+      </div>
+      <div>
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
