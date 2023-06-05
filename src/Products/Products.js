3c40e6acf012cacdcc05331e44e277901a406057
Andriikryl <andreyk557@gmail.com>
2023-06-05 20:10:22 +0300

    add product.js - component

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,6 +1,38 @@
 import React from "react";
 import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagFill } from "react-icons/bs";
 
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <section className="card-container">
+        <article className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="sho"
+          ></img>
+          <div className="card-details">
+            <h3 className="card-title">shoe</h3>
+            <div className="card-reviews"></div>
+          </div>
+          <div>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <span className="total-reviews">4</span>
+          </div>
+          <div className="card-price">
+            <div className="price">
+              <del>$300</del>
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
+  );
 }
